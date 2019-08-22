b9d85d9a834d26a8dd3ed53251f333a5537a17b5
natali62040 <54206199+natali62040@users.noreply.github.com>
2019-08-22 20:59:27 +0300

    create class loginToEbay

--- first-project/src/test/java/com/telran/selenium/OpenGoogleTest.java
+++ first-project/src/test/java/com/telran/selenium/OpenGoogleTest.java
@@ -1,19 +1,32 @@
 package com.telran.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class OpenGoogleTest {
-    WebDriver driver;
+import java.util.concurrent.TimeUnit;
+
+public class OpenGoogleTest extends TestBase{
+
     @Test
-    public void setUp() throws InterruptedException {
-        driver = new ChromeDriver();
+    public void searchJavaTest() throws InterruptedException{
         driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("java");
-      //  driver.navigate().to();
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("java"+ Keys.ENTER);
+        //  driver.navigate().to();
+
         Thread.sleep(7000);
-        driver.quit();
     }
+//   @Test
+    //  public void clickImFillingLucky(){
+    //       new Actions(driver).moveToElement(driver.findElement(By.name("btnI"))).click().perform();
+
+    //}
+
 }
